f08e89472917a87ddf79671fe96c68c12f8f5e2b
Folkert van Heusden <mail@vanheusden.com>
2025-09-22 19:37:37 +0200

    changes

--- polyglot-multi-merge.cpp
+++ polyglot-multi-merge.cpp
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	uint64_t                 min_n = 0;
 	std::string              out;
 	std::vector<std::string> files;
-	std::map<uint64_t, std::map<uint16_t, uint64_t> > work;
+	std::map<uint64_t, std::map<uint16_t, uint32_t> > work;
 
 	for(int i=1; i<argc; i++) {
 		if (strcmp(argv[i], "-in") == 0)
@@ -57,9 +57,10 @@ int main(int argc, char *argv[])
 			if (it == work.end())
 				work.insert({ hash, { { htons(entry.move), 1 } } });
 			else {
-				auto mit = it->second.find(htons(entry.move));
+				uint16_t move = ntohs(entry.move);
+				auto mit = it->second.find(move);
 				if (mit == it->second.end())
-					it->second.insert({ htons(entry.move), 1 });
+					it->second.insert({ move, 1 });
 				else
 					mit->second++;
 			}
@@ -71,7 +72,7 @@ int main(int argc, char *argv[])
 	}
 
 	// check counts
-	uint64_t max_count   = 0;
+	uint32_t max_count   = 0;
 	uint64_t total_count = 0;
 	uint64_t total_sq    = 0;
 	uint64_t n = 0;
@@ -84,38 +85,43 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	printf("Max. count: %" PRIu64 "\n", max_count);
+	printf("Max. count: %u\n", max_count);
 	double avg = total_count / double(n);
-	printf("Avg. count: %.2f\n", avg);
-	printf("Std. dev. : %.2f\n", sqrt(total_sq / double(n) - avg * avg));
+	printf("Avg. count: %.6f\n", avg);
+	printf("Std. dev. : %.6f\n", sqrt(total_sq / double(n) - avg * avg));
 	if (max_count > 65535) {
-		uint64_t scale = (max_count + 32766) / 65532;
-		printf("Dividing by %" PRIu64 "\n", scale);
+		uint32_t scale = (max_count + 32766) / 65532;
+		printf("Dividing by %u\n", scale);
 		for(auto & e: work) {
 			for(auto &me: e.second)
 				me.second /= scale;
 		}
 	}
 
-	// write
-	uint64_t      skipped = 0;
-	std::ofstream output(out, std::ios::binary);
-	for(auto & e: work) {
-		for(auto &me: e.second) {
-			if (me.second >= min_n) {
-				entry_t e_out { };
-				e_out.key    = NTOHLL(e.first);
-				e_out.move   = ntohs(me.first);
-				e_out.weight = ntohs(me.second);
+	if (files.size() > 1) {
+		// write
+		uint64_t      skipped = 0;
+		std::ofstream output(out, std::ios::binary);
+		for(auto & e: work) {
+			for(auto &me: e.second) {
+				if (me.second >= min_n) {
+					entry_t e_out { };
+					e_out.key    = NTOHLL(e.first);
+					e_out.move   = htons(me.first);
+					e_out.weight = htons(me.second);
 
-				output.write(reinterpret_cast<const char *>(&e_out), sizeof e_out);
-			}
-			else {
-				skipped++;
+					output.write(reinterpret_cast<const char *>(&e_out), sizeof e_out);
+				}
+				else {
+					skipped++;
+				}
 			}
 		}
+		printf("Total number of moves: %" PRIu64 ", written: %" PRIu64 "\n", n, n - skipped);
 	}
-	printf("Total number of moves: %" PRIu64 ", written: %" PRIu64 "\n", n, n - skipped);
+	else {
+		printf("Total number of moves: %" PRIu64 "\n", n);
+	}
 
 	return 0;
 }
